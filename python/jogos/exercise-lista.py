cf8b14d486c9d59970a79044462c11be3f268e7d
João Mesquita <joaobmesquita1@gmail.com>
2025-12-27 20:58:26 -0200

    finishe lista exercise

--- python/jogos/exercise-lista.py
+++ python/jogos/exercise-lista.py
@@ -3,6 +3,8 @@ maior = lista[0]
 menor = lista[0]
 pares = []
 total_ocorrencia_primeiro_elemento = 0
+total = 0
+soma_valores_negativos = 0
 
 for value in lista:
     #Maior - Menor
@@ -18,8 +20,19 @@ for value in lista:
     #ocorrências primeiro elemento
     if(value == lista[0]):
         total_ocorrencia_primeiro_elemento += 1
+    
+    #media
+    total += value
+
+    #valoes negativos
+    if(value < 0):
+        soma_valores_negativos += value
+    
+media = total/len(lista)
 
 
 print("Lista:\nMaior: {}\nMenor: {}".format(maior, menor))
 print("Pares: {}".format(pares))
 print("Total Ocorrência primeiro elemento: {}".format(total_ocorrencia_primeiro_elemento))
+print("Media: {}".format(total))
+print("Soma valoes negativos: {}".format(soma_valores_negativos))
