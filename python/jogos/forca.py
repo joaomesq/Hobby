71df00b9545207e69291a689124e9b5de6bd17a1
João Mesquita <joaobmesquita1@gmail.com>
2025-12-28 20:20:41 -0200

    jogando para dentro de função a lógica do jogo

--- python/jogos/forca.py
+++ python/jogos/forca.py
@@ -1,37 +1,38 @@
-print("***************************************")
-print("***   Bem vindo ao joga da Forva    ***")
-print("***************************************")
 
-secret_word = 'banana'
-letras_acertadas = []
-erros = 0
-acertou = False
-enforcou = False
-
-#montando espaços
-while(len(secret_word) > len(letras_acertadas)):
-    letras_acertadas.append("_")
-
-
-print(letras_acertadas)
-while(not acertou and not enforcou):
-    chute = input("Qual letra?: ")
-    posicao = 0
-
-    if(chute.lower() in secret_word.lower()):
-        for letra in secret_word:
-            if(letra.lower() == chute.lower()):
-                letras_acertadas[posicao] = chute
-            
-            posicao += 1
-    else:
-        erros += 1
+def jogar():
+    print("***************************************")
+    print("***   Bem vindo ao joga da Forva    ***")
+    print("***************************************")
     
-    acertou = "_" not in letras_acertadas
-    enforcou = erros == 6
+    secret_word = 'banana'
+    letras_acertadas = []
+    erros = 0
+    acertou = False
+    enforcou = False
+    
+    #montando espaços
+    while(len(secret_word) > len(letras_acertadas)):
+        letras_acertadas.append("_")
+        
     print(letras_acertadas)
+    while(not acertou and not enforcou):
+        chute = input("Qual letra?: ")
+        posicao = 0
+        
+        if(chute.lower() in secret_word.lower()):
+            for letra in secret_word:
+                if(letra.lower() == chute.lower()):
+                    letras_acertadas[posicao] = chute
+            
+                posicao += 1
+        else:
+            erros += 1
+        
+        acertou = "_" not in letras_acertadas
+        enforcou = erros == 6
+        print(letras_acertadas)
 
-if(acertou):
-    print("Você ganhou")
-else:
-    print("Você perdeu!")
+    if(acertou):
+        print("Você ganhou")
+    else:
+        print("Você perdeu!")
--- python/jogos/jogo-adivinha.py
+++ python/jogos/jogo-adivinha.py
@@ -1,37 +1,39 @@
-print("***************************************")
-print("*         Jogo de Adivinhação         *")
-print("***************************************")
 
-nivel = int(input("Escolha o nivél(1, 2, 3): "))
+def jogar():
+    print("***************************************")
+    print("*         Jogo de Adivinhação         *")
+    print("***************************************")
 
-secrect_number = 21
-if(nivel == 1):
-    tentativas = 20
-elif(nivel == 2):
-    tentativas = 10
-elif(nivel == 3):
-    tentativas = 5
-else:
-    nivel = 5
+    nivel = int(input("Escolha o nivél(1, 2, 3): "))
 
-for rodada in range(1, tentativas + 1):
-    print("\nRodade {} de {}".format(rodada, tentativas))
-    
-    chute = int(input("\nDigite um número: "))
-    print("Voce digitou {}\n".format(chute))
-
-    acertou = secrect_number == chute
-    maior = secrect_number < chute
-    menor = secrect_number > chute
-    
-    if(acertou):
-        print("Você acertou!")
-        break
-    elif(maior):
-        print("Você errou! Chute maior que número secreto")
-    elif(menor): 
-        print("Você errou! Chute menor que número secreto")
+    secrect_number = 21
+    if(nivel == 1):
+        tentativas = 20
+    elif(nivel == 2):
+        tentativas = 10
+    elif(nivel == 3):
+        tentativas = 5
     else:
-        print("Você errou!")
+        nivel = 5
 
-print("\nFim do jogo!")
+    for rodada in range(1, tentativas + 1):
+        print("\nRodade {} de {}".format(rodada, tentativas))
+    
+        chute = int(input("\nDigite um número: "))
+        print("Voce digitou {}\n".format(chute))
+
+        acertou = secrect_number == chute
+        maior = secrect_number < chute
+        menor = secrect_number > chute
+    
+        if(acertou):
+            print("Você acertou!")
+            break
+        elif(maior):
+            print("Você errou! Chute maior que número secreto")
+        elif(menor): 
+            print("Você errou! Chute menor que número secreto")
+        else:
+            print("Você errou!")
+
+    print("\nFim do jogo!")
