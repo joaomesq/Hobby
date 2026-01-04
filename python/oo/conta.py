2ecc375a7b7d26ca78a7eda7cd0099f5a18b1c9a
João Mesquita <joaob.mesquita1@gmail.com>
2026-01-04 00:09:15 -0300

    adicionando historico

--- python/oo/conta.py
+++ python/oo/conta.py
@@ -1,25 +1,43 @@
+import datetime
+
+class Historico:
+    def __init__(self):
+        self.data_abertura = datetime.datetime.today()
+        self.transacoes = []
+
+    def imprimir(self):
+        print("Data abertura: {}".format(self.data_abertura))
+        for t in self.transacoes:
+            print(" - {}".format(t))
+
+
 class Conta:
     def __init__(self, numero, cliente, saldo):
         self.numero = numero
         self.titular = cliente
         self.saldo = saldo
+        self.historico = Historico()
 
     def depositar(self, valor):
         self.saldo += valor
+        self.historico.transacoes.append("Deposito de {}".format(valor))
     
     def sacar(self, valor):
         if(self.saldo < valor):
             return False
 
         self.saldo -= valor
+        self.historico.transacoes.append("Saque de {}".format(valor))
         return True
     
     def extrato(self):
         print("Número: {}\nSaldo: {}\n".format(self.numero, self.saldo))
+        self.historico.transacoes.append("Tirou extrato - saldo de {}".format(self.saldo))
     
     def transfere_para(self, destino, valor):
         if(self.sacar(valor)):
             destino.depositar(valor)
+            self.historico.transacoes.append("Transferência de {} para {}".format(valor, destino.numero))
             return True
         else:
             return False
