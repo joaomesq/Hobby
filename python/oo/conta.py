a28b95e1572b48f9512439944f6c323c0c4a9893
João Mesquita <joaob.mesquita1@gmail.com>
2026-01-12 18:41:51 -0300

    defindo os atributos como privado e criando as respectivas @properts

--- python/oo/conta.py
+++ python/oo/conta.py
@@ -12,37 +12,64 @@ class Historico:
 
 
 class Conta:
-    def __init__(self, numero, cliente, saldo):
-        self.numero = numero
-        self.titular = cliente
-        self.saldo = saldo
-        self.historico = Historico()
+
+    identificador = 1
+
+    __slots__ = ['_numero', '_titular', '_saldo', '_limite', '_historico']
+
+    def __init__(self, numero, cliente, saldo, limite = 160000.00):
+        self._numero = numero
+        self._titular = cliente
+        self._saldo = saldo
+        self._limite = limite
+        self._historico = Historico()
+        self._identificador = Conta.identificador
+        Conta.identificador += 1
+    
+    @property
+    def limite(self):
+        return self._titular
+
+    @limite.setter
+    def limite(self, valor):
+        self._limite = valor
+    
+    @property
+    def numero(self):
+        return self._numero
+    
+    @numero.setter
+    def numero(self, valor):
+        self._numero = valor
+    
+    def get_titular(self):
+        return self._titular
 
     def depositar(self, valor):
         if(valor <= 0):
             print("O valor precisa ser superior a zero")
             return False
 
-        self.saldo += valor
-        self.historico.transacoes.append("Deposito de {}".format(valor))
+        self._saldo += valor
+        self._historico.transacoes.append("Deposito de {}".format(valor))
     
     def sacar(self, valor):
-        if(self.saldo < valor):
+        if(self._saldo < valor):
             print("Saldo insuficiente")
             return False
 
-        self.saldo -= valor
-        self.historico.transacoes.append("Saque de {}".format(valor))
+        self._saldo -= valor
+        self._historico.transacoes.append("Saque de {}".format(valor))
         return True
     
     def extrato(self):
-        print("Número: {}\nSaldo: {}\n".format(self.numero, self.saldo))
-        self.historico.transacoes.append("Tirou extrato - saldo de {}".format(self.saldo))
+        print("Número: {}\nSaldo: {}\n".format(self._numero, self._saldo))
+        self._historico.transacoes.append("Tirou extrato - saldo de {}".format(self._saldo))
     
     def transfere_para(self, destino, valor):
         if(self.sacar(valor)):
             destino.depositar(valor)
-            self.historico.transacoes.append("Transferência de {} para {}".format(valor, destino.numero))
+            self._historico.transacoes.append("Transferência de {} para {}".format(valor, destino._numero))
             return True
         else:
             return False
--- python/oo/teste.py
+++ python/oo/teste.py
@@ -4,5 +4,34 @@ from cliente import Cliente
 cliente = Cliente(nome = "João", sobrenome = "Mesquita", nif = "1234")
 conta = Conta(saldo = 1000, cliente = cliente, numero = 123)
 
-conta.depositar(-2456)
-conta.historico.imprimir()
+conta.depositar(100)
+conta._numero = 29
+conta.numero = 90
+print(conta._numero)
+conta._historico.imprimir()
+
+class Pessoa:
+    _total_pessoas = 0
+
+    __slots__ = ['_nome', '__idade']
+
+    def __init__(self, nome, idade):
+        self.__idade = idade
+        self._nome = nome
+        type(self)._total_pessoas += 1
+
+    @property
+    def idade(self):
+        return self.__idade
+    
+    @idade.setter
+    def idade(self, valor):
+        if(valor <= 0):
+            print("Idade não pode ser menor ou igual a zero")
+            return False
+        else:
+            self.__idade = valor
+    
+    @classmethod
+    def get_total_pessoas(cls):
+        return cls._total_pessoas
