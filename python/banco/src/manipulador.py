b64b462a96a46b6a0376cf642353b746c9210374
João Mesquita <joaob.mesquita1@gmail.com>
2026-01-27 14:38:30 -0300

    create ManipuladorTributaveis

---
+++ python/banco/src/manipulador.py
@@ -0,0 +1,7 @@
+class ManipuladorTributaveis:
+    def calcular_impostos(self, lista_tributaveis):
+        total = 0
+        for t in lista_tributaveis:
+            total += t.get_valor_imposto()
+        
+        return total
