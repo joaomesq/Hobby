2e34f1769a8d9276d97eeda4edc28cce4c98d34f
João Mesquita <joaob.mesquita1@gmail.com>
2026-02-03 02:30:09 +0100

    Testando o manipulador de tributaveis

--- python/banco/src/manipulador.py
+++ python/banco/src/manipulador.py
@@ -5,3 +5,24 @@ class ManipuladorTributaveis:
             total += t.get_valor_imposto()
         
         return total
+
+
+if __name__ == "__main__":
+    from  conta import ContaCorrente
+    from seguro_de_vida import SeguroDeVida
+
+    c1 = ContaCorrente(saldo = 1000, numero = 1, titular = "Mesquita" )
+    c2 = ContaCorrente(saldo = 200, numero = 2, titular = "Euclides")
+    seguro = SeguroDeVida(valor = 100, titular = "Augusto", numero_apolice = 1)
+
+    lt = []
+    lt.append(c1)
+    lt.append(c2)
+    lt.append(seguro)
+
+    print(c1.get_valor_imposto())
+    print(c2.get_valor_imposto())
+    print(seguro.get_valor_imposto())
+
+    manipulador = ManipuladorTributaveis()
+    print(manipulador.calcular_impostos(lt))
