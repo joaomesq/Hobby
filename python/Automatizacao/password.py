8cefa9ed76108bd59d02ad552d7a77a8594d59b2
João Mesquita <joaob.mesquita1@gmail.com>
2026-06-21 02:02:45 +0100

    criando pequeno teste para armazenar senhas

--- python/Automatizacao/password.py
+++ python/Automatizacao/password.py
@@ -1,5 +1,23 @@
 #!/usr/bin/env python3
+# Um programa para repositórios de senha, não seguro ainda.
 
-import sys
+import sys, pyperclip
 
-print(sys.argv)
+PASSWORDS = {
+    'email': "Joaob.mesquita1@gmail.com", 'blog': 'cafeDigita', 'luggage': '1234',
+    'test@example.com': "password"
+}
+
+if(len(sys.argv) < 2):
+    print('Usage: python password.py [account] - copy account password')
+    sys.exit()
+
+account = sys.argv[1]# primeiro argumento é o nome da conta
+
+if account in PASSWORDS:
+    pyperclip.copy(PASSWORDS[account])
+    senha = pyperclip.paste()
+    print(f"Password for {account} copy to clipboard")
+else:
+    print(f"There is no account named {account}")
+    
